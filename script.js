8aa5c8aa6a5adb213133fcffcedfdfe54c887712
tanuja-suvarnakar <tanusuwarnakar123@gmail.com>
2025-11-22 12:04:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#element', {
+    strings: ['Java Developer', 'Backend Specialist', 'Spring Boot Enthusiast', 'REST API Builder'],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true,
+});
